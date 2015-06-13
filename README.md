89d36406bae50e93cdcf81d21ad7a6e75ae1764d
Martynas Mickevičius <mmartynas@gmail.com>
2015-06-13 17:19:48 +0300

    Dependency management

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-#Vilnius Self-service bicycle map
+# Vilnius Self-service bicycle map
 
 The map was made using Leaflet.js mapping library and JCDecaux self-service bicycle API data and thus requires an API key, that can be requested at https://developer.jcdecaux.com.
 To retrieve the API data, a small backend server has been written in Flask that makes the API call and caches the data.
 
 Although this application is tailored to display bicycle data for Vilnius, it is trivial to change the backend so that other cities with JCDecaux self-service bicycle scheme, such as Paris, Brussels or Dublin, could be shown.
+
+## Run on your machine
+
+```
+virtualenv --prompt="(dviraciai)" -p python3.4 ENV
+source ENV/bin/activate
+python setup.py install
+JCD_API_KEY=<JCDecaux API Key from developer.jcdecaux.com> python app.py
+```
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, Response, render_template
 import json
-import requests, requests_cache
+import requests
+import requests_cache
+import os
 
 QUERY_URL = 'https://api.jcdecaux.com/vls/v1/stations?contract=Vilnius&apiKey=%s'
-API_KEY = ''
+API_KEY = os.getenv('JCD_API_KEY')
 
 if not API_KEY:
     raise Exception('''No JCDecaux API key was supplied''')
@@ -16,10 +18,11 @@ requests_cache.install_cache(cache_name='jcdecaux_cache', backend='sqlite', expi
 def index():
     return render_template('index.html')
 
+
 @app.route('/apidata')
 def jcdecaux_cycledata():
     r = requests.get(QUERY_URL % API_KEY)
-    return Response(json.dumps(r.json()),  mimetype='application/json')
+    return Response(json.dumps(r.json()), mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
